efbf2a7035b9d9a45c209a774e513a339b81438f
androidcodehunter <systemuser123>
2017-05-28 01:27:40 -0700

    update gradle.

--- app/src/main/java/sharif/shakeitup/room/WordDatabase.java
+++ app/src/main/java/sharif/shakeitup/room/WordDatabase.java
@@ -1,5 +1,6 @@
 package sharif.shakeitup.room;
 
+import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
@@ -10,9 +11,10 @@ import sharif.shakeitup.db.model.*;
  * Created by Sharif-PC on 5/22/2017.
  */
 
+@Database(entities = {Word.class}, version = 1)
 public abstract class WordDatabase extends RoomDatabase {
 
     static final String DATABASE_NAME = "word-db";
-
     public abstract WordDao wordDao();
+
 }
